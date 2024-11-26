04f564880f48d4dd4a75e63dd7b37ed9c0ac0ca1
feernandogpt <https://github.com/fernandogpt/feernandogpt.git>
2024-11-26 19:02:31 -0300

    segundo commit

---
+++ src/sala.java
@@ -0,0 +1,17 @@
+public class sala {
+    private int largura;
+    private int altura;
+
+    public sala(int largura, int altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+}
